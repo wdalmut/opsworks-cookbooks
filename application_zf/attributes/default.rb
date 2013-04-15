54e18145d90f8528952ba35e74a2f20eba32405a
Walter Dal Mut <walter.dalmut@gmail.com>
2013-04-15 19:48:16 +0200

    Added application_zf

---
+++ application_zf/attributes/default.rb
@@ -0,0 +1,29 @@
+#
+# Author:: Walter Dal Mut (<walter.dalmut@gmail.com>)
+# Cookbook Name:: application_zf
+# Attributes:: application_zf
+#
+# Copyright 2013, Walter Dal Mut.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# General settings
+default['zend']['version'] = "latest"
+default['zend']['dir'] = "/var/www/zend"
+default['zend']['server_name'] = node['fqdn']
+default['zend']['server_aliases'] = [node['fqdn']]
+default['zend']['modules'] = ["Application"]
+default['zend']['composer']['packages'] = []
+default['zend']['dev']['version'] = "dev-master"
+default['zend']['skeleton']['repository'] = "https://github.com/zendframework/ZendSkeletonApplication"
---
+++ application_zf/metadata.rb
@@ -0,0 +1,60 @@
+name             "application_zf"
+maintainer       "Walter Dal Mut"
+maintainer_email "walter.dalmut@gmail.com"
+license          "Apache 2.0"
+description      "Installs/Configures ZendSkeletonApplication"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.5"
+
+recipe "application_zf", "Installs and configures Zend Skeleton Application on a single system"
+recipe "application_zf::dev_tools", "Install ZendDeveloperTools"
+
+%w{ php openssl build-essential apt }.each do |cb|
+  depends cb
+end
+
+depends "apache2", ">= 0.99.4"
+
+%w{ debian ubuntu }.each do |os|
+  supports os
+end
+
+attribute "application_zf/version",
+  :display_name => "ZendSkeletonApplication download version",
+  :description => "Version of ZendSkeletonApplication to download from the ZendSkeletonApplication site or 'latest' for the current release.",
+  :default => "latest"
+
+attribute "application_zf/dir",
+  :display_name => "ZendSkeletonApplication installation directory",
+  :description => "Location to place ZendSkeletonApplication files.",
+  :default => "/var/www/zend"
+
+attribute "application_zf/zend/server_name",
+  :display_name => "ZendSkeletonApplication Server Name",
+  :description => "ZendSkeletonApplication Server Name",
+  :default => "FQDN"
+  
+attribute "application_zf/zend/server_aliases",
+  :display_name => "ZendSkeletonApplication Server Aliases",
+  :description => "ZendSkeletonApplication Server Aliases",
+  :default => ["FQDN"]
+  
+attribute "application_zf/zend/modules",
+  :display_name => "ZendSkeletonApplication Enable modules",
+  :description => "Array of Module names that you want to enable in your application.config.php",
+  :default => ["Application"]
+
+attribute "application_zf/zend/composer/packages",
+  :display_name => "ZendSkeletonApplication extra composer packages",
+  :description => "Array of Module names that you want to enable in your application.config.php",
+  :default => []
+
+attribute "application_zf/zend/dev/version",
+  :display_name => "ZendDeveloperTools version",
+  :description => "Set the version of ZendDeveloperTools module",
+  :default => "dev-master"
+
+attribute "application_zf/zend/skeleton/repository",
+  :display_name => "Use personal ZF skeleton application",
+  :description => "Set the repository URL of your ZendSkeletonApplication",
+  :default => "latest"
